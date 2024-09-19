4cc131642400f674dd0f280f5ccef7fbcb9f4fc7
ebrucihan <ebrchnnn123@gmail.com>
2024-09-19 20:20:15 +0300

    javascript works done.

---
+++ Week-6/JavaScript-Works2/modules.js
@@ -0,0 +1,13 @@
+export const name = "Jhon";
+export const surname = "Doe";
+export const age = 21;
+
+export function sayhi() {
+  console.log("Merhaba");
+}
+
+export default {
+  name,
+  surname,
+  age,
+};
